88287cd0fe830772ae3d825d4c55757b42e46869
A_Brave_Panda <nichtompkins72@gmail.com>
2018-01-19 22:43:21 -0500

    Added Incorrect Arguments String

--- src/me/gl1tchcraft/api/messages/GCMessagesAPI.java
+++ src/me/gl1tchcraft/api/messages/GCMessagesAPI.java
@@ -3,6 +3,7 @@ package me.gl1tchcraft.api.messages;
 public class GCMessagesAPI {
 	
 	public static String noPermission = "§7You don't have permission to do that!";
+	public static String incorrectArgs = "§cIncorrect Arguments!";
 	
 
 }
